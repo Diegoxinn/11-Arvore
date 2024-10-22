28a42daaf034004c404481dfbe47efecc4c13a59
Diego <diego07_ferreira@hotmail.com>
2024-10-22 11:43:59 -0300

    Tarefa realizada.

--- ArvoreBinaria/ArvoreBinaria/ArvoreBinaria.cpp
+++ ArvoreBinaria/ArvoreBinaria/ArvoreBinaria.cpp
@@ -179,7 +179,23 @@ void exibirElementosArvore(NO* no)
 
 void buscarElementoArvore(NO* no, int valor)
 {
+	if (no == NULL) {
+		cout << "Elemento nao encontrado." << endl;
+		return;
+	}
 
+	if (no->valor == valor) {
+		cout << "Elemento " << valor << " encontrado na arvore." << endl;
+		return;
+	}
+
+
+	if (valor < no->valor) {
+		buscarElementoArvore(no->esq, valor);
+	}
+	else {
+		buscarElementoArvore(no->dir, valor);
+	}
 
 }
 
